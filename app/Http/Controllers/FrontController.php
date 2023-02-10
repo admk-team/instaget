99eb2520451d0d08a44f2c58ef28d51161e5431d
wadood <wanikhan919@gmail.com>
2023-02-10 15:15:45 +0500

    working on instagram api

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -90,7 +90,7 @@ class FrontController extends Controller
                 'code' => $request->code,
         ]);
         $res=$response->json();
-        return Redirect::to('https://graph.instagram.com/me/media?fields=id,caption&access_token='.$res['access_token']);
+        return Redirect::to('https://graph.instagram.com/me/media?fields=id,media_type,media_url,username&access_token='.$res['access_token']);
 
         // $media = Redirect::to('https://graph.instagram.com/'.$res['user_id'].'?fields=id,username&access_token='.$res['access_token']);
         
