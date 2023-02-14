4582801205636a473c147c241416d7ce249c2536
wadood <wanikhan919@gmail.com>
2023-02-14 10:46:22 +0500

    order

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -107,7 +107,7 @@ class FrontController extends Controller
     }
     public function place_order(Request $request){
         $package_id=session()->get('PACKAGE_ID');
-        return $package = Package::with('sub_category.category.service')->where('id',$package_id)->first();
+        $package = Package::with('sub_category.category.service')->where('id',$package_id)->first();
         return view('front.payment',compact('package'));
     }
 }
