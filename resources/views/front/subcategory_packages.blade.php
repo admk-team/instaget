6e3ddc1cd0bd75810f66b6ebb16eef22f232ff52
wadood <wanikhan919@gmail.com>
2023-02-15 19:02:29 +0500

    add qty to service page

--- resources/views/front/subcategory_packages.blade.php
+++ resources/views/front/subcategory_packages.blade.php
@@ -156,9 +156,9 @@
                   $firstpackage = DB::table('packages')->where('sub_category_id' , $subcategory->id)->first();
                 @endphp
                 <div class="p-4 justify-content-center d-flex">
-                  <h4 class="sale-price first-box-sale-price">{{ $firstpackage->sale_price ?? $firstpackage->original_price ?? '' }} </h4> &nbsp;
+                  <h4 class="sale-price first-box-sale-price">{{ floor($firstpackage->sale_price) ?? floor($firstpackage->original_price) ?? '' }}  </h4><span class="pt-2" style="font-size: 22px;font-weight: 800">원</span> &nbsp;
                   @if (isset($firstpackage->sale_price) && $firstpackage->sale_price != '')
-                  <del class="orignal-price first-box-orignal-price"> {{ $firstpackage->original_price ?? '' }} </del>
+                  <del class="orignal-price first-box-orignal-price"> {{ floor($firstpackage->original_price) ?? '' }}  </del><del class="pt-2" style="font-size: 20px;font-weight: 600;color: lightgray"> 원</del>
                   @endif
                 </div>
                 <div class="p-3 justify-content-center d-none d-md-block">
