70e90041bea784b2fb3ad2320cb167a98387072f
wadood <wanikhan919@gmail.com>
2023-02-17 16:27:17 +0500

    conflict resolved

--- routes/web.php
+++ routes/web.php
@@ -68,14 +68,11 @@ Route::middleware('guest:admin')->group(function () {
 Route::get('/cmd/{cmd}', [FrontController::class, 'cmd']);
 //Front Routes//
 Route::post('/instagram', [FrontController::class, 'instagram'])->name('front.instagram.getpost');
-<<<<<<< HEAD
 Route::any('/fetch/instagram/post', [FrontController::class, 'fetch_post'])->name('front.instagram.fetchpost');
-=======
 Route::post('/fetch/instagram/post', [FrontController::class, 'fetch_post'])->name('front.instagram.fetchpost');
 Route::get('/guest/instagram/post', [FrontController::class, 'guest_post'])->name('front.instagram.guestpost');
 Route::get('/user/register', [FrontController::class, 'user_signup'])->name('front.user.register');
 Route::get('/user/login', [FrontController::class, 'user_login'])->name('front.user.login');
->>>>>>> a4ab47b8e6e028aa31a6625b31937de32e214518
 Route::name('front.')->group(function () {
     Route::get('/', [FrontController::class, 'index'])->name('index');
     Route::get('/instagram/callback', [FrontController::class, 'callback']);
