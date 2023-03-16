73824b22b74de59d0fcdd64ffe5ef8454064f062
rizwan ali <webd54402@gmail.com>
2023-03-16 16:41:41 +0500

    changes

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -18,10 +18,10 @@ class EventServiceProvider extends ServiceProvider
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
-        // \SocialiteProviders\Manager\SocialiteWasCalled::class => [
-        //     // add your listeners (aka providers) here
-        //     'SocialiteProviders\\Naver\\NaverExtendSocialite@handle',
-        // ],
+        \SocialiteProviders\Manager\SocialiteWasCalled::class => [
+            // add your listeners (aka providers) here
+            'SocialiteProviders\\Naver\\NaverExtendSocialite@handle',
+        ],
     ];
 
     /**
